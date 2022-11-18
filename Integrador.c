5a080b10a3ad760dea46a8abf3c5459055a58509
NicolasBadino <116890168+NicolasBadino@users.noreply.github.com>
2022-11-18 08:58:23 -0300

    Add files via upload

---
+++ Integrador.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+//#include <string.h> libreria de string que creo que no podemos usar.
+
+int cant;
+char i;
+int CalLong(char palabra[30]);
+int Mayor;
+char palabraM[30];
+int main(void) {
+  int Resultado;
+  printf("Bienvenido, por favor ingrese un tope de palabras\n");
+  scanf("%d", &cant);
+  for (i = 0; i < cant; i++) {
+    char palabra[30];
+    printf("Ingrese su palabra por favor\n");
+    scanf("%s", palabra);
+    Resultado = CalLong(palabra);
+    if (Resultado > Mayor) {
+      Mayor = Resultado;
+      for (int j = 0; j < 29; j++) {
+        palabraM[j] = palabra[j];
+      }
+    }
+  }
+  printf("La palabra [%s] es la más larga, tiene %d letras\n", palabraM, Mayor);
+  return 0;
+}
+int CalLong(char palabra[30]) {
+  int letras = 0;
+  for (int i = 0; palabra[i] != '\0'; i++) {
+    if (palabra[i] != ' ')
+      letras++;
+  }
+  return (letras);
+}
